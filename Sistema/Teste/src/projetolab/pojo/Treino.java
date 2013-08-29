18c1f5dc8a9628518a0d3802cc65296391de3535
ritter01 <ritter.zc@gmail.com>
2013-08-29 08:46:31 -0300

    Classe Treino
    Classe treino implementada

--- Sistema/Teste/src/projetolab/pojo/Treino.java
+++ Sistema/Teste/src/projetolab/pojo/Treino.java
@@ -1,5 +1,36 @@
 package projetolab.pojo;
 
 public class Treino {
+	
+	private String exercicio;
+	private int serie;
+	private int repeticao;
+	
+	public Treino(String exercicio, int serie, int repeticao) {
+		super();
+		this.exercicio = exercicio;
+		this.serie = serie;
+		this.repeticao = repeticao;
+	}
+
+	public String getExercicio() {
+		return exercicio;
+	}
+
+	public int getSerie() {
+		return serie;
+	}
+
+	public int getRepeticao() {
+		return repeticao;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Treino [exercicio=%s, serie=%s, repeticao=%s]",
+				exercicio, serie, repeticao);
+	}
+	
+	
 
 }
