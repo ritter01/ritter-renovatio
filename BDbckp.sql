84cf07e25a41b1ddb820c52cf8ccd6ffa07deb21
ritter01 <ritter.zc@gmail.com>
2013-08-29 08:34:10 -0300

    Issue #6 Issue #7
    Treinos cadastrados Issue #6
    Endereço corrigido Issue #7

--- BDbckp.sql
+++ BDbckp.sql
@@ -1,121 +1,25 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 9.2.2
--- Dumped by pg_dump version 9.2.2
--- Started on 2013-08-15 11:35:35
-
-SET statement_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
---
--- TOC entry 1926 (class 1262 OID 49925)
--- Name: projetolab; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE projetolab WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'Portuguese_Brazil.1252' LC_CTYPE = 'Portuguese_Brazil.1252';
-
-
-ALTER DATABASE projetolab OWNER TO postgres;
-
-\connect projetolab
-
-SET statement_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
---
--- TOC entry 170 (class 3079 OID 11727)
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- TOC entry 1929 (class 0 OID 0)
--- Dependencies: 170
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- TOC entry 168 (class 1259 OID 49926)
--- Name: alunos; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE alunos (
-    nome character varying(60),
-    idade numeric NOT NULL,
-    endereco character varying(40)
-);
-
-
-ALTER TABLE public.alunos OWNER TO postgres;
-
---
--- TOC entry 169 (class 1259 OID 49932)
--- Name: treinos; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE treinos (
-    exercicios character varying(60),
-    series numeric,
-    repeticoes numeric
-);
-
-
-ALTER TABLE public.treinos OWNER TO postgres;
-
---
--- TOC entry 1920 (class 0 OID 49926)
--- Dependencies: 168
--- Data for Name: alunos; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO alunos VALUES ('Roberto', 26, 'RuaFeliciano1269');
-INSERT INTO alunos VALUES ('Paula', 20, 'AvenidaGregoriano853AP02');
-INSERT INTO alunos VALUES ('Jorge', 36, 'RuaCarlosAlmeida963');
-INSERT INTO alunos VALUES ('Julia', 19, 'AvenidaSarmen153');
-
-
---
--- TOC entry 1921 (class 0 OID 49932)
--- Dependencies: 169
--- Data for Name: treinos; Type: TABLE DATA; Schema: public; Owner: postgres
---
+﻿create table Alunos
+(
+NOME varchar(60),
+IDADE NUMERIC NOT NULL,
+Endereco varchar(60));
 
 
 
---
--- TOC entry 1928 (class 0 OID 0)
--- Dependencies: 5
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
+insert into Alunos
+VALUES ('Roberto',26,'Rua Feliciano 1269');
+insert into Alunos
+VALUES ('Paula',20,'Avenida Gregoriano 853 AP 02');
+insert into Alunos
+VALUES ('Jorge',36,'Rua Carlos Almeida 963');
+insert into Alunos
+VALUES ('Julia',19,'Avenida Sarmen 153');
 
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
--- Completed on 2013-08-15 11:35:35
-
---
--- PostgreSQL database dump complete
---
-
+insert into Treinos
+VALUES ('Remada Aberta',3,12);
+insert into Treinos
+VALUES ('Supino Reto',3,12);
+insert into Treinos
+VALUES ('Triceps Corda',3,12);
+insert into Treinos
+VALUES ('Elevacao Lateral',3,12);
